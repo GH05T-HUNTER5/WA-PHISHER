9c73208c54839df8c5581bfb257ef72fda6227b0
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-06-29 16:08:58 +0530

    Add files via upload

---
+++ WA-PHISHER.py
@@ -0,0 +1,19 @@
+#!/bin/user/python
+# Code : GH05T HUNTER5
+# Do not upload this tool to your GitHub without my consent
+# Collaborator : GH05T-HUNTER5
+import os
+import sys
+os.system("clear")
+print ("")
+print (" ██╗    ██╗ █████╗       ██████╗ ██╗  ██╗██╗███████╗██╗  ██╗███████╗██████╗")
+print (" ██║    ██║██╔══██╗      ██╔══██╗██║  ██║██║██╔════╝██║  ██║██╔════╝██╔══██╗")
+print (" ██║ █╗ ██║███████║█████╗██████╔╝███████║██║███████╗███████║█████╗  ██████╔╝")
+print (" ██║███╗██║██╔══██║╚════╝██╔═══╝ ██╔══██║██║╚════██║██╔══██║██╔══╝  ██╔══██╗")
+print (" ╚███╔███╔╝██║  ██║      ██║     ██║  ██║██║███████║██║  ██║███████╗██║  ██║")
+print ("  ╚══╝╚══╝ ╚═╝  ╚═╝      ╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝")
+print ("  Authors : GH05T-HUNTER5                                    { Ver : 1.0.1 }")
+print ("")
+os.system("bash air.sh")
+os.system("bash .tools/main.sh")
+exit ()
